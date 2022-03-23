2c82eaf8f482e78c9993f4fd81dd5f6538bd68b4
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-03-23 19:12:43 -0400

    Display fish with basic lighting
    - Added Uniform buffer objects

--- NPR-SPH/Main.cpp
+++ NPR-SPH/Main.cpp
@@ -18,16 +18,50 @@
 #include "LoadTexture.h"   //Functions for creating OpenGL textures from image files
 #include "VideoMux.h"      //Functions for saving videos
 
-const int init_window_width = 1024;
-const int init_window_height = 1024;
-const char* const window_title = "Template";
+int window_width = 1024;
+int window_height = 1024;
+float aspect = 1.0f;
+const char* const window_title = "NPR-SPH";
 
 static const std::string vertex_shader("template_vs.glsl");
 static const std::string fragment_shader("template_fs.glsl");
 GLuint shader_program = -1;
 
 static const std::string mesh_name = "Amago0.obj";
-static const std::string texture_name = "AmagoT.bmp";
+
+//This structure mirrors the uniform block declared in the shader
+struct SceneUniforms
+{
+    glm::mat4 PV;	//camera projection * view matrix
+    glm::vec4 eye_w;	//world-space eye position
+    glm::vec4 light_w = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f); //world-space light position
+} SceneData;
+
+struct MaterialUniforms
+{
+    glm::vec4 ka = glm::vec4(0.35f, 0.0f, 0.0f, 1.0f); // Ambient material color
+    glm::vec4 kd = glm::vec4(glm::vec3(0.5f), 1.0f); // Diffuse material color
+    glm::vec4 ks = glm::vec4(glm::vec3(0.5f), 1.0f); // Specular material color
+    float shininess = 20.0f; //specular exponent
+} MaterialData;
+
+//IDs for the buffer objects holding the uniform block data
+GLuint scene_ubo = -1;
+GLuint material_ubo = -1;
+
+namespace UboBinding
+{
+    //These values come from the binding value specified in the shader block layout
+    int scene = 0;
+    int material = 1;
+}
+
+//Locations for the uniforms which are not in uniform blocks
+namespace UniformLocs
+{
+    int M = 0; //model matrix
+    int time = 1;
+}
 
 GLuint texture_id = -1; //Texture map for mesh
 MeshData mesh_data;
@@ -40,148 +74,155 @@ bool recording = false;
 
 void draw_gui(GLFWwindow* window)
 {
-   //Begin ImGui Frame
-   ImGui_ImplOpenGL3_NewFrame();
-   ImGui_ImplGlfw_NewFrame();
-   ImGui::NewFrame();
+    //Begin ImGui Frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
-   //Draw Gui
-   ImGui::Begin("Debug window");                       
-   if (ImGui::Button("Quit"))                          
-   {
-      glfwSetWindowShouldClose(window, GLFW_TRUE);
-   }    
+    //Draw Gui
+    ImGui::Begin("Debug window");
+    if (ImGui::Button("Quit"))
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
 
-   const int filename_len = 256;
-   static char video_filename[filename_len] = "capture.mp4";
+    const int filename_len = 256;
+    static char video_filename[filename_len] = "capture.mp4";
 
-   ImGui::InputText("Video filename", video_filename, filename_len);
-   ImGui::SameLine();
-   if (recording == false)
-   {
-      if (ImGui::Button("Start Recording"))
-      {
-         int w, h;
-         glfwGetFramebufferSize(window, &w, &h);
-         recording = true;
-         start_encoding(video_filename, w, h); //Uses ffmpeg
-      }
-      
-   }
-   else
-   {
-      if (ImGui::Button("Stop Recording"))
-      {
-         recording = false;
-         finish_encoding(); //Uses ffmpeg
-      }
-   }
+    ImGui::InputText("Video filename", video_filename, filename_len);
+    ImGui::SameLine();
+    if (recording == false)
+    {
+        if (ImGui::Button("Start Recording"))
+        {
+            int w, h;
+            glfwGetFramebufferSize(window, &w, &h);
+            recording = true;
+            start_encoding(video_filename, w, h); //Uses ffmpeg
+        }
 
-   ImGui::SliderFloat("View angle", &angle, -glm::pi<float>(), +glm::pi<float>());
-   ImGui::SliderFloat("Scale", &scale, -10.0f, +10.0f);
+    }
+    else
+    {
+        if (ImGui::Button("Stop Recording"))
+        {
+            recording = false;
+            finish_encoding(); //Uses ffmpeg
+        }
+    }
 
-   ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-   ImGui::End();
+    ImGui::SliderFloat("View angle", &angle, -glm::pi<float>(), +glm::pi<float>());
+    ImGui::SliderFloat("Scale", &scale, -10.0f, +10.0f);
 
-   //static bool show_test = false;
-   //ImGui::ShowDemoWindow(&show_test);
+    ImGui::SliderFloat4("Light position", &SceneData.light_w.x, -1.0f, 1.0f);
 
-   //End ImGui Frame
-   ImGui::Render();
-   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // Control Material colors
+    ImGui::ColorEdit3("Material Ambient Color", &MaterialData.ka.r, 0);
+    ImGui::ColorEdit3("Material Diffuse Color", &MaterialData.kd.r, 0);
+    ImGui::ColorEdit3("Material Specular Color", &MaterialData.ks.r, 0);
+
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    ImGui::End();
+
+    //static bool show_test = false;
+    //ImGui::ShowDemoWindow(&show_test);
+
+    //End ImGui Frame
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 // This function gets called every time the scene gets redisplayed
 void display(GLFWwindow* window)
 {
-   //Clear the screen to the color previously specified in the glClearColor(...) call.
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   
-   glm::mat4 M = glm::rotate(angle, glm::vec3(0.0f, 1.0f, 0.0f))*glm::scale(glm::vec3(scale*mesh_data.mScaleFactor));
-   glm::mat4 V = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-   glm::mat4 P = glm::perspective(glm::pi<float>()/4.0f, 1.0f, 0.1f, 100.0f);
+    //Clear the screen to the color previously specified in the glClearColor(...) call.
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-   glUseProgram(shader_program);
+    SceneData.eye_w = glm::vec4(0.0f, 1.0f, 3.0f, 1.0f);
+    glm::mat4 M = glm::rotate(angle, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::scale(glm::vec3(scale * mesh_data.mScaleFactor));
+    glm::mat4 V = glm::lookAt(glm::vec3(SceneData.eye_w.x, SceneData.eye_w.y, SceneData.eye_w.z), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 P = glm::perspective(glm::pi<float>() / 4.0f, aspect, 0.1f, 100.0f);
+    SceneData.PV = P * V;
 
-   glActiveTexture(GL_TEXTURE0);
-   glBindTexture(GL_TEXTURE_2D, texture_id);
-   int tex_loc = glGetUniformLocation(shader_program, "diffuse_tex");
-   glUniform1i(tex_loc, 0); // we bound our texture to texture unit 0
+    glUseProgram(shader_program);
 
+    //Set uniforms
+    glUniformMatrix4fv(UniformLocs::M, 1, false, glm::value_ptr(M));
 
-   //Get location for shader uniform variable
-   glm::mat4 PVM = P*V*M;
-   int PVM_loc = glGetUniformLocation(shader_program, "PVM");
-   glUniformMatrix4fv(PVM_loc, 1, false, glm::value_ptr(PVM));
+    glBindBuffer(GL_UNIFORM_BUFFER, scene_ubo); //Bind the OpenGL UBO before we update the data.
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(SceneData), &SceneData); //Upload the new uniform values.
+    //glBindBuffer(GL_UNIFORM_BUFFER, 0); //unbind the ubo
 
-   glBindVertexArray(mesh_data.mVao);
-   glDrawElements(GL_TRIANGLES, mesh_data.mSubmesh[0].mNumIndices, GL_UNSIGNED_INT, 0);
-   //For meshes with multiple submeshes use mesh_data.DrawMesh(); 
+    glBindBuffer(GL_UNIFORM_BUFFER, material_ubo); //Bind the OpenGL UBO before we update the data.
+    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(MaterialUniforms), &MaterialData); //Upload the new uniform values.
+    //glBindBuffer(GL_UNIFORM_BUFFER, 0); //unbind the ubo
 
-   draw_gui(window);
+    glBindVertexArray(mesh_data.mVao);
+    glDrawElements(GL_TRIANGLES, mesh_data.mSubmesh[0].mNumIndices, GL_UNSIGNED_INT, 0);
 
-   if (recording == true)
-   {
-      glFinish();
-      glReadBuffer(GL_BACK);
-      int w, h;
-      glfwGetFramebufferSize(window, &w, &h);
-      read_frame_to_encode(&rgb, &pixels, w, h);
-      encode_frame(rgb);
-   }
+    draw_gui(window);
 
-   /* Swap front and back buffers */
-   glfwSwapBuffers(window);
+    if (recording == true)
+    {
+        glFinish();
+        glReadBuffer(GL_BACK);
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        read_frame_to_encode(&rgb, &pixels, w, h);
+        encode_frame(rgb);
+    }
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
 }
 
 void idle()
 {
-   float time_sec = static_cast<float>(glfwGetTime());
+    float time_sec = static_cast<float>(glfwGetTime());
 
-   //Pass time_sec value to the shaders
-   int time_loc = glGetUniformLocation(shader_program, "time");
-   glUniform1f(time_loc, time_sec);
+    //Pass time_sec value to the shaders
+    glUniform1f(UniformLocs::time, time_sec);
 }
 
 void reload_shader()
 {
-   GLuint new_shader = InitShader(vertex_shader.c_str(), fragment_shader.c_str());
+    GLuint new_shader = InitShader(vertex_shader.c_str(), fragment_shader.c_str());
 
-   if (new_shader == -1) // loading failed
-   {
-      glClearColor(1.0f, 0.0f, 1.0f, 0.0f); //change clear color if shader can't be compiled
-   }
-   else
-   {
-      glClearColor(0.35f, 0.35f, 0.35f, 0.0f);
+    if (new_shader == -1) // loading failed
+    {
+        glClearColor(1.0f, 0.0f, 1.0f, 0.0f); //change clear color if shader can't be compiled
+    }
+    else
+    {
+        glClearColor(0.35f, 0.35f, 0.35f, 0.0f);
 
-      if (shader_program != -1)
-      {
-         glDeleteProgram(shader_program);
-      }
-      shader_program = new_shader;
-   }
+        if (shader_program != -1)
+        {
+            glDeleteProgram(shader_program);
+        }
+        shader_program = new_shader;
+    }
 }
 
 //This function gets called when a key is pressed
 void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-   std::cout << "key : " << key << ", " << char(key) << ", scancode: " << scancode << ", action: " << action << ", mods: " << mods << std::endl;
+    std::cout << "key : " << key << ", " << char(key) << ", scancode: " << scancode << ", action: " << action << ", mods: " << mods << std::endl;
 
-   if(action == GLFW_PRESS)
-   {
-      switch(key)
-      {
-         case 'r':
-         case 'R':
-            reload_shader();     
-         break;
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+        case 'r':
+        case 'R':
+            reload_shader();
+            break;
 
-         case GLFW_KEY_ESCAPE:
+        case GLFW_KEY_ESCAPE:
             glfwSetWindowShouldClose(window, GLFW_TRUE);
-         break;     
-      }
-   }
+            break;
+        }
+    }
 }
 
 //This function gets called when the mouse moves over the window.
@@ -196,75 +237,95 @@ void mouse_button(GLFWwindow* window, int button, int action, int mods)
     //std::cout << "button : "<< button << ", action: " << action << ", mods: " << mods << std::endl;
 }
 
+void resize(GLFWwindow* window, int width, int height)
+{
+    window_width = width;
+    window_height = height;
+
+    glViewport(0, 0, width, height); //Set viewport to cover entire framebuffer
+    aspect = float(width) / float(height); //Set aspect ratio used in view matrix calculation
+}
+
 //Initialize OpenGL state. This function only gets called once.
 void initOpenGL()
 {
-   glewInit();
+    glewInit();
 
-   //Print out information about the OpenGL version supported by the graphics driver.	
-   std::cout << "Vendor: "       << glGetString(GL_VENDOR)                    << std::endl;
-   std::cout << "Renderer: "     << glGetString(GL_RENDERER)                  << std::endl;
-   std::cout << "Version: "      << glGetString(GL_VERSION)                   << std::endl;
-   std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION)  << std::endl;
-   glEnable(GL_DEPTH_TEST);
+    //Print out information about the OpenGL version supported by the graphics driver.	
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    glEnable(GL_DEPTH_TEST);
 
-   reload_shader();
-   mesh_data = LoadMesh(mesh_name);
-   texture_id = LoadTexture(texture_name);
+    reload_shader();
+    mesh_data = LoadMesh(mesh_name);
+
+    //Create and initialize uniform buffers
+    glGenBuffers(1, &scene_ubo);
+    glBindBuffer(GL_UNIFORM_BUFFER, scene_ubo);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(SceneUniforms), nullptr, GL_STREAM_DRAW); //Allocate memory for the buffer, but don't copy (since pointer is null).
+    glBindBufferBase(GL_UNIFORM_BUFFER, UboBinding::scene, scene_ubo); //Associate this uniform buffer with the uniform block in the shader that has the same binding.
+
+    glGenBuffers(1, &material_ubo);
+    glBindBuffer(GL_UNIFORM_BUFFER, material_ubo);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(MaterialUniforms), &MaterialData, GL_STREAM_DRAW); //Allocate memory for the buffer, but don't copy (since pointer is null).
+    glBindBufferBase(GL_UNIFORM_BUFFER, UboBinding::material, material_ubo); //Associate this uniform buffer with the uniform block in the shader that has the same binding.
+
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
 }
 
-
-
 //C++ programs start executing in the main() function.
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-   GLFWwindow* window;
+    GLFWwindow* window;
 
-   /* Initialize the library */
-   if (!glfwInit())
-   {
-      return -1;
-   }
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+        return -1;
+    }
 
-   /* Create a windowed mode window and its OpenGL context */
-   window = glfwCreateWindow(init_window_width, init_window_height, window_title, NULL, NULL);
-   if (!window)
-   {
-      glfwTerminate();
-      return -1;
-   }
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(window_width, window_height, window_title, NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-   //Register callback functions with glfw. 
-   glfwSetKeyCallback(window, keyboard);
-   glfwSetCursorPosCallback(window, mouse_cursor);
-   glfwSetMouseButtonCallback(window, mouse_button);
+    //Register callback functions with glfw. 
+    glfwSetKeyCallback(window, keyboard);
+    glfwSetCursorPosCallback(window, mouse_cursor);
+    glfwSetMouseButtonCallback(window, mouse_button);
+    glfwSetFramebufferSizeCallback(window, resize);
 
-   /* Make the window's context current */
-   glfwMakeContextCurrent(window);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-   initOpenGL();
-   
-   //Init ImGui
-   IMGUI_CHECKVERSION();
-   ImGui::CreateContext();
-   ImGui_ImplGlfw_InitForOpenGL(window, true);
-   ImGui_ImplOpenGL3_Init("#version 150");
+    initOpenGL();
 
-   /* Loop until the user closes the window */
-   while (!glfwWindowShouldClose(window))
-   {
-      idle();
-      display(window);
+    //Init ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
 
-      /* Poll for and process events */
-      glfwPollEvents();
-   }
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        idle();
+        display(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
     // Cleanup ImGui
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-   glfwTerminate();
-   return 0;
+    glfwTerminate();
+    return 0;
 }

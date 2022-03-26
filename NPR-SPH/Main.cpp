67ebcb2c45f8180ab39daa888019daa0d7638872
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-03-26 18:14:36 -0400

    Replaced code with TransformFeedback code

--- NPR-SPH/Main.cpp
+++ NPR-SPH/Main.cpp
@@ -1,5 +1,10 @@
 #include <windows.h>
 
+//When using this as a template, be sure to make these changes in the new project: 
+//1. In Debugging properties set the Environment to PATH=%PATH%;$(SolutionDir)\lib;
+//2. Change window_title below
+//3. Copy assets (mesh and texture) to new project directory
+
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
@@ -12,48 +17,53 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <vector>
 
 #include "InitShader.h"    //Functions for loading shaders from text files
-#include "LoadMesh.h"      //Functions for creating OpenGL buffers from mesh files
 #include "LoadTexture.h"   //Functions for creating OpenGL textures from image files
 #include "VideoMux.h"      //Functions for saving videos
+#include "DebugCallback.h"
+#include "UniformGui.h"
 
-int window_width = 1024;
-int window_height = 1024;
-float aspect = 1.0f;
-const char* const window_title = "NPR-SPH";
+const int init_window_width = 1024;
+const int init_window_height = 1024;
+const char* const window_title = "CGT 521 Final Project - NPR-SPH";
 
-static const std::string vertex_shader("template_vs.glsl");
-static const std::string fragment_shader("template_fs.glsl");
+static const std::string vertex_shader("npr-sph_vs.glsl");
+static const std::string fragment_shader("npr-sph_fs.glsl");
 GLuint shader_program = -1;
 
-static const std::string mesh_name = "Amago0.obj";
+float angle = 0.0f;
+float scale = 1.0f;
+float aspect = 1.0f;
+bool recording = false;
+
+//Ping-pong pairs of objects and buffers since we don't have simultaneous read/write access to VBOs.
+GLuint vao[2] = { -1, -1 };
+GLuint vbo[2] = { -1, -1 };
+const int num_particles = 10000;
+GLuint tfo[2] = { -1, -1 }; //transform feedback objects
+//Nmaes of the VS out variables that should be captured by transform feedback
+const char* xform_feedback_varyings[] = { "pos_out", "vel_out", "age_out" };
+
+//These indices get swapped every frame to perform the ping-ponging
+int read_index = 0;  //initially read from VBO_ID[0]
+int write_index = 1; //initially write to VBO_ID[1]
 
 //This structure mirrors the uniform block declared in the shader
 struct SceneUniforms
 {
     glm::mat4 PV;	//camera projection * view matrix
     glm::vec4 eye_w;	//world-space eye position
-    glm::vec4 light_w = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f); //world-space light position
 } SceneData;
-
-struct MaterialUniforms
-{
-    glm::vec4 ka = glm::vec4(0.35f, 0.0f, 0.0f, 1.0f); // Ambient material color
-    glm::vec4 kd = glm::vec4(glm::vec3(0.5f), 1.0f); // Diffuse material color
-    glm::vec4 ks = glm::vec4(glm::vec3(0.5f), 1.0f); // Specular material color
-    float shininess = 20.0f; //specular exponent
-} MaterialData;
 
 //IDs for the buffer objects holding the uniform block data
 GLuint scene_ubo = -1;
-GLuint material_ubo = -1;
 
 namespace UboBinding
 {
     //These values come from the binding value specified in the shader block layout
     int scene = 0;
-    int material = 1;
 }
 
 //Locations for the uniforms which are not in uniform blocks
@@ -63,12 +73,12 @@ namespace UniformLocs
     int time = 1;
 }
 
-GLuint texture_id = -1; //Texture map for mesh
-MeshData mesh_data;
-
-float angle = 0.0f;
-float scale = 1.0f;
-bool recording = false;
+namespace AttribLocs
+{
+    int pos = 0;
+    int vel = 1;
+    int age = 2;
+}
 
 //For an explanation of this program's structure see https://www.glfw.org/docs/3.3/quick.html 
 
@@ -78,6 +88,8 @@ void draw_gui(GLFWwindow* window)
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
+
+    UniformGui(shader_program);
 
     //Draw Gui
     ImGui::Begin("Debug window");
@@ -114,13 +126,6 @@ void draw_gui(GLFWwindow* window)
     ImGui::SliderFloat("View angle", &angle, -glm::pi<float>(), +glm::pi<float>());
     ImGui::SliderFloat("Scale", &scale, -10.0f, +10.0f);
 
-    ImGui::SliderFloat4("Light position", &SceneData.light_w.x, -1.0f, 1.0f);
-
-    // Control Material colors
-    ImGui::ColorEdit3("Material Ambient Color", &MaterialData.ka.r, 0);
-    ImGui::ColorEdit3("Material Diffuse Color", &MaterialData.kd.r, 0);
-    ImGui::ColorEdit3("Material Specular Color", &MaterialData.ks.r, 0);
-
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     ImGui::End();
 
@@ -138,10 +143,10 @@ void display(GLFWwindow* window)
     //Clear the screen to the color previously specified in the glClearColor(...) call.
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    SceneData.eye_w = glm::vec4(0.0f, 1.0f, 3.0f, 1.0f);
-    glm::mat4 M = glm::rotate(angle, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::scale(glm::vec3(scale * mesh_data.mScaleFactor));
-    glm::mat4 V = glm::lookAt(glm::vec3(SceneData.eye_w.x, SceneData.eye_w.y, SceneData.eye_w.z), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    glm::mat4 P = glm::perspective(glm::pi<float>() / 4.0f, aspect, 0.1f, 100.0f);
+    SceneData.eye_w = glm::vec4(0.0f, 0.0f, 3.0f, 1.0f);
+    glm::mat4 M = glm::rotate(angle, glm::vec3(1.0f, 0.0f, 0.0f)) * glm::scale(glm::vec3(scale));
+    glm::mat4 V = glm::lookAt(glm::vec3(SceneData.eye_w), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::mat4 P = glm::perspective(glm::pi<float>() / 4.0f, 1.0f, 0.1f, 100.0f);
     SceneData.PV = P * V;
 
     glUseProgram(shader_program);
@@ -151,16 +156,48 @@ void display(GLFWwindow* window)
 
     glBindBuffer(GL_UNIFORM_BUFFER, scene_ubo); //Bind the OpenGL UBO before we update the data.
     glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(SceneData), &SceneData); //Upload the new uniform values.
-    //glBindBuffer(GL_UNIFORM_BUFFER, 0); //unbind the ubo
+    glBindBuffer(GL_UNIFORM_BUFFER, 0); //unbind the ubo
 
-    glBindBuffer(GL_UNIFORM_BUFFER, material_ubo); //Bind the OpenGL UBO before we update the data.
-    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(MaterialUniforms), &MaterialData); //Upload the new uniform values.
-    //glBindBuffer(GL_UNIFORM_BUFFER, 0); //unbind the ubo
+    glDepthMask(GL_FALSE);
+    //BEGIN TRANSFORM FEEDBACK RENDERING
+    const bool TFO_SUPPORTED = true;
 
-    glBindVertexArray(mesh_data.mVao);
-    glDrawElements(GL_TRIANGLES, mesh_data.mSubmesh[0].mNumIndices, GL_UNSIGNED_INT, 0);
+    if (TFO_SUPPORTED == true)
+    {
+        //Bind the current write transform feedback object.
+        glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, tfo[write_index]);
+    }
+    else
+    {
+        //Binding the transform feedback object recalls the buffer range state shown below. If
+        //your system does not support transform feedback objects you can uncomment the following lines.
+        const GLint pos_varying = 0;
+        const GLint vel_varying = 1;
+        const GLint age_varying = 2;
 
-    draw_gui(window);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, pos_varying, vbo[write_index]);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, vel_varying, vbo[write_index]);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, age_varying, vbo[write_index]);
+    }
+
+    //Prepare the pipeline for transform feedback
+    glBeginTransformFeedback(GL_POINTS);
+    glBindVertexArray(vao[read_index]);
+    glDrawArrays(GL_POINTS, 0, num_particles);
+    glEndTransformFeedback();
+
+    if (TFO_SUPPORTED == true)
+    {
+        //Bind the current write transform feedback object.
+        glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
+    }
+
+    //Ping-pong the buffers.
+    read_index = 1 - read_index;
+    write_index = 1 - write_index;
+
+    //END TRANSFORM FEEDBACK RENDERING
+    glDepthMask(GL_TRUE);
 
     if (recording == true)
     {
@@ -172,16 +209,36 @@ void display(GLFWwindow* window)
         encode_frame(rgb);
     }
 
+    draw_gui(window);
+
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
 }
 
 void idle()
 {
-    float time_sec = static_cast<float>(glfwGetTime());
+    //time_sec = static_cast<float>(glfwGetTime());
+
+    static float time_sec = 0.0f;
+    time_sec += 1.0f / 60.0f;
 
     //Pass time_sec value to the shaders
     glUniform1f(UniformLocs::time, time_sec);
+}
+
+void transform_feedback_relink(GLuint shader_program)
+{
+    //You need to specify which varying variables will capture transform feedback values.
+    glTransformFeedbackVaryings(shader_program, 3, xform_feedback_varyings, GL_INTERLEAVED_ATTRIBS);
+
+    //Must relink the program after specifying transform feedback varyings.
+    glLinkProgram(shader_program);
+    int status;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        printProgramLinkError(shader_program);
+    }
 }
 
 void reload_shader()
@@ -201,13 +258,15 @@ void reload_shader()
             glDeleteProgram(shader_program);
         }
         shader_program = new_shader;
+
+        transform_feedback_relink(shader_program);
     }
 }
 
 //This function gets called when a key is pressed
 void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    std::cout << "key : " << key << ", " << char(key) << ", scancode: " << scancode << ", action: " << action << ", mods: " << mods << std::endl;
+    //std::cout << "key : " << key << ", " << char(key) << ", scancode: " << scancode << ", action: " << action << ", mods: " << mods << std::endl;
 
     if (action == GLFW_PRESS)
     {
@@ -239,17 +298,22 @@ void mouse_button(GLFWwindow* window, int button, int action, int mods)
 
 void resize(GLFWwindow* window, int width, int height)
 {
-    window_width = width;
-    window_height = height;
-
-    glViewport(0, 0, width, height); //Set viewport to cover entire framebuffer
-    aspect = float(width) / float(height); //Set aspect ratio used in view matrix calculation
+    //Set viewport to cover entire framebuffer
+    glViewport(0, 0, width, height);
+    //Set aspect ratio used in view matrix calculation
+    aspect = float(width) / float(height);
 }
+
+#define BUFFER_OFFSET( offset )   ((GLvoid*) (offset))
 
 //Initialize OpenGL state. This function only gets called once.
 void initOpenGL()
 {
     glewInit();
+
+#ifdef _DEBUG
+    RegisterCallback();
+#endif
 
     //Print out information about the OpenGL version supported by the graphics driver.	
     std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
@@ -258,8 +322,70 @@ void initOpenGL()
     std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
     glEnable(GL_DEPTH_TEST);
 
+    //Enable alpha blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE); //additive alpha blending
+    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //semitransparent alpha blending
+
+    //Enable gl_PointCoord in shader
+    glEnable(GL_POINT_SPRITE);
+    //Allow setting point size in fragment shader
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+    //create TFOs
+    glGenTransformFeedbacks(2, tfo);
+
+    //all attribs are initially zero
+    std::vector<float> zeros(7 * num_particles, 0.0f); //particle positions, velocities, ages
+
+    //These are the indices in the array passed to glTransformFeedbackVaryings (const char *vars[] = { "pos_out", "vel_out", "age_out" };)
+    const GLint pos_varying = 0;
+    const GLint vel_varying = 1;
+    const GLint age_varying = 2;
+
+    //create VAOs and VBOs
+    glGenVertexArrays(2, vao);
+    glGenBuffers(2, vbo);
+
+    const int stride = 7 * sizeof(float);
+    const int pos_offset = 0;
+    const int vel_offset = sizeof(glm::vec3);
+    const int age_offset = 2 * sizeof(glm::vec3);
+    const int pos_size = num_particles * sizeof(glm::vec3);
+    const int vel_size = num_particles * sizeof(glm::vec3);
+    const int age_size = num_particles * sizeof(float);
+
+    for (int i = 0; i < 2; i++)
+    {
+        //Create VAO and VBO with interleaved attributes
+        glBindVertexArray(vao[i]);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo[i]);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * zeros.size(), zeros.data(), GL_DYNAMIC_COPY);
+
+        glEnableVertexAttribArray(AttribLocs::pos);
+        glVertexAttribPointer(AttribLocs::pos, 3, GL_FLOAT, false, stride, BUFFER_OFFSET(pos_offset));
+
+        glEnableVertexAttribArray(AttribLocs::vel);
+        glVertexAttribPointer(AttribLocs::vel, 3, GL_FLOAT, false, stride, BUFFER_OFFSET(vel_offset));
+
+        glEnableVertexAttribArray(AttribLocs::age);
+        glVertexAttribPointer(AttribLocs::age, 1, GL_FLOAT, false, stride, BUFFER_OFFSET(age_offset));
+
+        //Tell the TFO where each varying variable should be written in the VBO.
+
+        glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, tfo[i]);
+
+        //Specify VBO to write into
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, pos_varying, vbo[i]);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, vel_varying, vbo[i]);
+        glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, age_varying, vbo[i]);
+
+        glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
+        glBindVertexArray(0); //unbind vao
+        glBindBuffer(GL_ARRAY_BUFFER, 0); //unbind vbo
+    }
+
     reload_shader();
-    mesh_data = LoadMesh(mesh_name);
 
     //Create and initialize uniform buffers
     glGenBuffers(1, &scene_ubo);
@@ -267,13 +393,10 @@ void initOpenGL()
     glBufferData(GL_UNIFORM_BUFFER, sizeof(SceneUniforms), nullptr, GL_STREAM_DRAW); //Allocate memory for the buffer, but don't copy (since pointer is null).
     glBindBufferBase(GL_UNIFORM_BUFFER, UboBinding::scene, scene_ubo); //Associate this uniform buffer with the uniform block in the shader that has the same binding.
 
-    glGenBuffers(1, &material_ubo);
-    glBindBuffer(GL_UNIFORM_BUFFER, material_ubo);
-    glBufferData(GL_UNIFORM_BUFFER, sizeof(MaterialUniforms), &MaterialData, GL_STREAM_DRAW); //Allocate memory for the buffer, but don't copy (since pointer is null).
-    glBindBufferBase(GL_UNIFORM_BUFFER, UboBinding::material, material_ubo); //Associate this uniform buffer with the uniform block in the shader that has the same binding.
-
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
 }
+
+
 
 //C++ programs start executing in the main() function.
 int main(int argc, char** argv)
@@ -286,8 +409,12 @@ int main(int argc, char** argv)
         return -1;
     }
 
+#ifdef _DEBUG
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#endif
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(window_width, window_height, window_title, NULL, NULL);
+    window = glfwCreateWindow(init_window_width, init_window_height, window_title, NULL, NULL);
     if (!window)
     {
         glfwTerminate();

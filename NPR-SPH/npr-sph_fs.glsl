f99e989393ba0df3dada4d9bb0553d25cf4c7dac
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-03-31 16:29:33 -0400

    Removed unused code

--- NPR-SPH/npr-sph_fs.glsl
+++ NPR-SPH/npr-sph_fs.glsl
@@ -6,18 +6,3 @@ void main ()
 {
     frag_color = vec4(0.2f, 0.3f, 0.8f, 1.0f);
 }
-
-/* // Working rain shader
-#version 440
-
-layout(location = 1) uniform float time;
-
-out vec4 fragcolor; //the output color for this fragment    
-
-uniform vec4 particle_color_rgb = vec4(0.2f, 0.3f, 0.8f, 1.0f);
-
-void main(void)
-{   
-   fragcolor = particle_color_rgb;
-}
-*/
--- NPR-SPH/npr-sph_vs.glsl
+++ NPR-SPH/npr-sph_vs.glsl
@@ -9,7 +9,7 @@ layout(std140, binding = 0) uniform SceneUniforms
    vec4 eye_w;	//world-space eye position
 };
 
-layout (location = 2) in vec2 position;
+layout (location = 2) in vec4 position;
 
 out gl_PerVertex
 {
@@ -19,76 +19,6 @@ out gl_PerVertex
 
 void main ()
 {
-    gl_Position = PV*M*vec4(position, 0.0f, 1.0f);;
+    gl_Position = PV * M * position;
     gl_PointSize = 4.0f;
 }
-
-/* // Working rain shader
-#version 440
-
-layout(location = 0) uniform mat4 M;
-layout(location = 1) uniform float time;
-
-layout(std140, binding = 0) uniform SceneUniforms
-{
-   mat4 PV;	//camera projection * view matrix
-   vec4 eye_w;	//world-space eye position
-};
-
-layout(location = 0) in vec3 pos_attrib;
-layout(location = 1) in vec3 vel_attrib;
-layout(location = 2) in vec3 for_attrib;
-layout(location = 3) in float rho_attrib;
-layout(location = 4) in float pres_attrib;
-layout(location = 5) in float age_attrib;
-
-//write into the first bound tfo
-layout(xfb_buffer = 0) out;
-
-//interleave attrib order was pos, vel, force, rho, pressure, age
-//3 floats + 3 floats + 3 floats + 1 float + 1 float + 1 float = 12 floats -> stride = 12*sizeof(float) = 48 bytes
-
-layout(xfb_offset = 0, xfb_stride = 48) out vec3 pos_out; 
-layout(xfb_offset = 12, xfb_stride = 48) out vec3 vel_out;
-layout(xfb_offset = 24, xfb_stride = 48) out vec3 for_out;
-layout(xfb_offset = 36, xfb_stride = 48) out float rho_out;
-layout(xfb_offset = 40, xfb_stride = 48) out float pres_out;
-layout(xfb_offset = 44, xfb_stride = 48) out float age_out;
-
-vec3 v0(vec3 p); //Basic velocity field
-float rand(vec2 co); // pseudorandom number
-
-const float dt = 0.01f;
-const float drag = 0.85f;
-
-void main(void)
-{
-	//Draw current particles from vbo
-	gl_Position = PV*M*vec4(pos_attrib, 1.0f);
-	gl_PointSize = 4.0;
-
-	//Compute particle attributes for next frame
-	vel_out = v0(pos_attrib) * drag; // Calculate gravity and drag
-	pos_out = pos_attrib + vel_out * time * dt;
-	age_out = age_attrib - 1.0;
-
-	// Reset particles if they go far enough
-	if(length(pos_attrib) > 100.0f)
-	{
-		///vec2 seed = vec2(float(gl_VertexID), time * dt); //seed for the random number generator
-		//age_out = 500.0 + 200.0*rand(seed);
-		pos_out.y += 50.0f; // Reset y coordinate
-	}
-}
-
-vec3 v0(vec3 p)
-{
-	return vec3(0.0f, for_attrib.y * time * dt + p.y, 0.0f);
-	//return vec3(sin(-p.y*10.0+time/7.0-10.0), sin(-p.x*10.0+1.2*time+10.0), sin(+7.0*p.x -5.0*p.y + time));
-}
-
-float rand(vec2 co)
-{
-    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
-}
-*/

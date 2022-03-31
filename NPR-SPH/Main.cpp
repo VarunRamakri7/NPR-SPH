8a64918db562a1d7ce780a14e525f6418910b4bc
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-03-31 16:56:18 -0400

    Updaed glVertexAttribPointer stride

--- NPR-SPH/Main.cpp
+++ NPR-SPH/Main.cpp
@@ -361,8 +361,8 @@ void initOpenGL()
     glBindVertexArray(particle_position_vao);
 
     glBindBuffer(GL_ARRAY_BUFFER, particles_ssbo);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, nullptr); // Bind buffer containing particle positions to VAO
-    glEnableVertexAttribArray(0); // Enable attribute with location = 2 (vertex position) for VAO
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Particle), BUFFER_OFFSET(0)); // Bind buffer containing particle positions to VAO
+    glEnableVertexAttribArray(0); // Enable attribute with location = 0 (vertex position) for VAO
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);

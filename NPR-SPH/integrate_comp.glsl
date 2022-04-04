c8bc3c4ffc6de09e9d12366d9fa0b4fd5abf1a04
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-04-04 18:27:47 -0400

    Move particles as a waterfall

--- NPR-SPH/integrate_comp.glsl
+++ NPR-SPH/integrate_comp.glsl
@@ -4,7 +4,7 @@
 #define NUM_PARTICLES 1000
 
 // For calculations
-#define TIME_STEP 0.01f
+#define TIME_STEP 0.0001f
 #define WALL_DAMPING 0.3f
 
 layout (local_size_x = WORK_GROUP_SIZE, local_size_y = 1, local_size_z = 1) in;
@@ -18,9 +18,6 @@ struct Particle
     vec4 vel;
     vec4 force;
     vec4 extras; // 0 - rho, 1 - pressure, 2 - age
-    //float rho;
-    //float pres;
-    //float age;
 };
 
 layout(std430, binding = 0) buffer PARTICLES
@@ -33,32 +30,36 @@ void main()
     uint i = gl_GlobalInvocationID.x;
     if(i >= NUM_PARTICLES) return;
 
-    /*
     // Integrate all components
-    vec3 acceleration = particles[i].force.xyz / particles[i].rho;
+    /*vec3 acceleration = particles[i].force.xyz / particles[i].extras.x;
     vec3 new_vel = particles[i].vel.xyz + TIME_STEP * acceleration;
-    vec3 new_pos = particles[i].pos.xyz + TIME_STEP * new_vel;
+    vec3 new_pos = particles[i].pos.xyz + TIME_STEP * new_vel;*/
+
+    vec3 new_pos = particles[i].pos.xyz;
+    new_pos.y = particles[i].pos.y - 0.00981f;
 
     // Boundary conditions. Keep particlex within [-1, 1] in all axis
     if (new_pos.x < -1.2f)
     {
-        new_pos.x = -1.f;
-        new_vel.x *= -1.0f * WALL_DAMPING;
+        new_pos.x = -1.0f;
+        //new_vel.x *= -1.0f * WALL_DAMPING;
     }
     else if (new_pos.x > 1.2f)
     {
         new_pos.x = 1.0f;
-        new_vel.x *= -1.0f * WALL_DAMPING;
+        //new_vel.x *= -1.0f * WALL_DAMPING;
     }
     else if (new_pos.y < -1.2f)
     {
-        new_pos.y = -1.0f;
-        new_vel.y *= -1.0f * WALL_DAMPING;
+        new_pos.y = 1.0f;
+        //new_pos.y = -1.0f;
+        //new_vel.y *= -1.0f * WALL_DAMPING;
     }
     else if (new_pos.y > 1.2f)
     {
-        new_pos.y = 1.0f;
-        new_vel.y *= -1.0f * WALL_DAMPING;
+        new_pos.y = -1.0f;
+        //new_pos.y = 1.0f;
+        //new_vel.y *= -1.0f * WALL_DAMPING;
     }
     else if (new_pos.z < -1.2f)
     {
@@ -72,11 +73,10 @@ void main()
     }
 
     // Assign calculated values
-    particles[i].vel.xyz = new_vel;
+    //particles[i].vel.xyz = new_vel;
     particles[i].pos.xyz = new_pos;
-    */
-    
+        
     // Placeholder
     //particles[i].vel.xyz += particles[i].force.xyz * TIME_STEP;
-    particles[i].pos.y += -0.00981f;
+    //particles[i].pos.y += -0.00981f;
 }
